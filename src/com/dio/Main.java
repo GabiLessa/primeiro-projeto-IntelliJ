d568a704656acb541dbaa7d3d2cc160a888fd49b
GabiLessa <gabriellalessa77@gmail.com>
2023-03-10 22:05:25 -0300

    first commit

---
+++ src/com/dio/Main.java
@@ -0,0 +1,17 @@
+package com.dio;
+
+
+import com.dio.model.gato;
+
+public class Main {
+    public static void main(String[] args) {
+        gato gato = new gato();
+
+        System.out.println(gato);
+
+       /* int a = 5;
+        int b = 5;
+        System.out.println("Hello world! How are you?" + (a + b));*/
+    }
+
+}
